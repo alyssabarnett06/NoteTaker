8ce860aeba973b2880413891023a0353da18ae8b
Catherine Barnett <ca.barnett06@gmail.com>
2020-01-14 18:39:57 -0500

    fixing bugs

---
+++ public/server.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var fs = require("fs");
+var path = require("path");
+
+var notesInfo = require("./db.json");
+
+var app = express();
+var PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+// app.use(express.static('files'))
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/notes.html"));
+});
+
+app.post("/api/notes", function(req,res){
+    let newNote = req.body;
+    notesInfo.push(newNote);
+    addId();
+    let save = JSON.stringify(notesInfo);
+    fs.writeFileSync("db.json",save)
+
+    res.redirect('back');
+});
+
+app.delete("/api/notes/:id", function (req,res) {
+    const deleted = notesInfo.findIndex((i) => i.id == req.params.id);
+    notesInfo.splice(deleted, 1);
+    reWrite();
+    res.json(notesInfo);
+});
+
+function addId() {
+    notesInfo.forEach((element, i) => {
+        element.id = i + 1;
+    });
+};
+let reWrite = () => {
+    let newDB = JSON.stringify(notesInfo);
+    fs.writeFile('db.json', newDB, err => { if (err) throw err });
+};
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notesInfo);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
